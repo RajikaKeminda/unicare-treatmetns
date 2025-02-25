68ae62d2471b31a60f99dedb78bf9b6661a752ca
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-02-25 21:25:29 +0530

    feat : add header

--- frontend/app/test-name/page.tsx
+++ frontend/app/test-name/page.tsx
@@ -1,10 +1,11 @@
 import TestComponent from "@/components/layout/Test"
 import Footer from "@/components/layout/Footer"
-
+import Header from "@/components/layout/Header"
 
 export default function Test() {
   return (
     <>
+      <Header/>
       <TestComponent/>
       <Footer/>
     </>
